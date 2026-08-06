e4c03a9d45e66b286bc8a5dd245e43e24a368e86
pamuduNethisa004 <pamudunj2022@gmail.com>
2026-08-06 12:06:39 +0530

    Create customer controller and resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/customers');
 });
+
+Route::resource('customers', CustomerController::class);
